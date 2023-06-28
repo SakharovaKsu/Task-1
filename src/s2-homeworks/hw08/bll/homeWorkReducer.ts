84ed724e412ef85ff6100db931b9900a2fbf7c59
SakharovaKsu <nagis38@gmail.com>
2023-06-28 17:09:20 +0300

    Дописала логику для hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,19 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const sortFunUp = (a: UserType, b: UserType) => a.name > b.name ? 1 : -1
+            const sortFunDown = (a: UserType, b: UserType) => a.name < b.name ? 1 : -1
+            const compareFun = action.payload === 'up' ? sortFunUp : sortFunDown
+            return state.map(n => ({...n})).sort(compareFun)
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(u => u.age >= action.payload)
         }
         default:
             return state
     }
 }
+
