32c945ced6078e0bac864823f19a685586caee50
SakharovaKsu <nagis38@gmail.com>
2023-09-25 13:15:53 +0300

    Написание логики для change (для ползунков) и стилизация (HW11)

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -4,9 +4,8 @@ import {Slider, SliderProps} from '@mui/material'
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
+            color={'secondary'}
+            sx={{width: '147px'}}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
     )
